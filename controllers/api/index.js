4534acca999288573a17c9f841c44b4f6137986d
Kelly R Cano <krcano2001@gmail.com>
2021-11-17 10:27:51 -0800

    added card layout to dashboard handlebars file

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const projectRoutes = require('./jobRoutes');
+const jobRoutes = require('./jobRoutes');
 
 router.use('/users', userRoutes);
 router.use('/jobs', jobRoutes);
