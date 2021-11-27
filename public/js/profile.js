0746f62022935e830b4ca04d50e8903a03a2fce0
Kelly R Cano <krcano2001@gmail.com>
2021-11-26 20:04:43 -0800

    added id for job form handler

--- public/js/profile.js
+++ public/js/profile.js
@@ -3,16 +3,16 @@ const createJobFormHandler = async (event) => {
     event.preventDefault();
 
     // get values
-    const jobTitle = document.querySelector("#job-title").value.trim();
-    const jobCompany = document.querySelector("#")
+    const job_title = document.querySelector("#job-title").value.trim();
+    const job_company = document.querySelector("#job-company")
     const description = document.querySelector("#job-desc").value.trim();
     const salary = document.querySelector("#job-salary").value.trim();
 
-    if (jobTitle && description && salary) {
+    if (job_title && description && salary && job_company) {
         // create post request 
         const response = await fetch("api/users/profile", {
             method: "POST",
-            body: JSON.stringify({ email, password }),
+            body: JSON.stringify({ job_company, job_title, description,salary}),
             headers: { "Content-Type": "application/json"}
         });
     }
@@ -22,3 +22,5 @@ const createReviewFormHandler = async (event) => {
     // get values
 
 }
+
+document.querySelector('#job-submit').addEventListener('click', createJobFormHandler)
