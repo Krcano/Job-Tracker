0ddda73843dd095d8f96acc7bc2b9f1c94d933a6
Kelly R Cano <krcano2001@gmail.com>
2021-11-29 13:58:55 -0800

    fixed sign up functionality

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,67 +3,85 @@ const { Users, Jobs, Reviews } = require("../models");
 const withAuth = require("../utils/auth");
 
 router.get("/", async (req, res) => {
-    try {
-        const reviewsData = await Reviews.findAll({
-            include: [
-                {
-                    model: Users,
-                    attributes: ["first_name", "last_name"]
-                }
-            ]
-        })
+  try {
+    const reviewsData = await Reviews.findAll({
+      include: [
+        {
+          model: Users,
+          attributes: ["first_name", "last_name"],
+        },
+      ],
+    });
 
-        const reviews = reviewsData.map((review) => review.get({ plain: true }));
+    const reviews = reviewsData.map((review) => review.get({ plain: true }));
 
-        res.render("feed", {
-            reviews,
-            logged_in: req.session.logged_in
-        });
-    } catch (err) {
-        res.status(500).json(err);
-    }
+    res.render("feed", {
+      reviews,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get("/profile", async (req, res) => {
-    console.log("=============== homeRoute - get/profile ============");
-    try {
-        console.log(req.session.user_id);
-        const userData = await Users.findByPk(req.session.user_id, {
-            attributes: { exclude: ["password"] },
-            include: [
-                {
-                    model: Jobs,
-                    where: {
-                        users_id: req.session.user_id
-                    }
-                },
-                {
-                    model: Reviews,
-                    where: {
-                        users_id: req.session.user_id
-                    }
-                }
-            ]
-        });
-        console.log(userData)
-        const user = userData.get({ plain: true });
-        console.log(user)
-        res.render("profile", {
-            user,
-            logged_in: true
-        });
-    } catch (err) {
-        res.status(500).json(err);
+  console.log("=============== homeRoute - get/profile ============");
+  try {
+    console.log(req.session.user_id);
+
+    let userData = await Users.findByPk(req.session.user_id, {
+      attributes: { exclude: ["password"] },
+      include: [
+        {
+          model: Jobs,
+          where: {
+            users_id: req.session.user_id,
+          },
+        },
+        // {
+        //   model: Reviews,
+        //   where: {
+        //     users_id: req.session.user_id,
+        //   },
+        // },
+      ],
+    });
+
+    console.log("userData run 1")
+    console.log(userData);
+
+
+    if (userData === null) {
+      console.log("IN IF =======================")
+      userData = await Users.findByPk(req.session.user_id, {
+        attributes: { exclude: ["password"] },
+      });
+      console.log(userData);
     }
+
+
+
+
+    const user = userData.get({ plain: true });
+    console.log(user);
+    
+    
+    res.render("profile", {
+      user,
+      logged_in: true,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get("/login", (req, res) => {
-    console.log("============= homeRoute - get/login =============")
-    if (req.session.logged_in) {
-        res.redirect("/profile");
-        return;
-    }
-    res.render("login");
+  console.log("============= homeRoute - get/login =============");
+  if (req.session.logged_in) {
+    res.redirect("/profile");
+    return;
+  }
+  res.render("login");
 });
 
 module.exports = router;
