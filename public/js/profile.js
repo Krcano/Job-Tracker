545d99cc40ec173c9d5d61b2ab0f43ff743755b7
Kelly R Cano <krcano2001@gmail.com>
2021-11-29 13:29:08 -0800

    create review and create job fixed

--- public/js/profile.js
+++ public/js/profile.js
@@ -5,20 +5,20 @@ const createJobFormHandler = async (event) => {
 
 
     // get values
-    // const company = document.querySelector("#company").value.trim();
+    const company = document.querySelector("#company").value.trim();
     const title = document.querySelector("#job-title").value.trim();
-    // const location = document.querySelector("#location").value.trim();
+    const location = document.querySelector("#location").value.trim();
     const description = document.querySelector("#job-desc").value.trim();
     const salary = document.querySelector("#job-salary").value.trim();
-    // const url = document.querySelector("#url").value.trim();
-    // const status = document.querySelector("#status").value.trim();
+    const url = document.querySelector("#url").value.trim();
+    const status = document.querySelector("#status").value.trim();
 
     if (title && description && salary) {
         console.log("RUNNING POST REQUEST")
         // create post request, add commented out values to the body
         const response = await fetch("api/profile/createjob", {
             method: "POST",
-            body: JSON.stringify({ title, description, salary }),
+            body: JSON.stringify({company, title, location, description, salary, url, status }),
             headers: { "Content-Type": "application/json"}
         });
 
@@ -31,30 +31,30 @@ const createJobFormHandler = async (event) => {
     }
 };
 
-const createReviewFormHandler = async (event) => {
-    event.preventDefault();
+// const createReviewFormHandler = async (event) => {
+//     event.preventDefault();
 
-    // get values
-    const review_text = document.querySelector("#review-text").value.trim();
-    const review_name = document.querySelector("#review-name").value.trim();
+//     // get values
+//     const review_text = document.querySelector("#review-text").value.trim();
+//     const review_name = document.querySelector("#review-name").value.trim();
+//     const company =  document.querySelector("#company").value.trim();
+//     if (review_text && review_name && company) {
+//         const response = await fetch("api/profile/createreview", {
+//             method: "POST",
+//             body: JSON.stringify({ review_text, review_name, company }),
+//             headers: { "Content-Type": "application/json"}
+//         });
 
-    if (review_text && review_name) {
-        const response = await fetch("api/profile/createreview", {
-            method: "POST",
-            body: JSON.stringify({ review_text, review_name }),
-            headers: { "Content-Type": "application/json"}
-        });
+//         if (response.ok) {
+//             console.log("-------- ACCOUNT CREATED ----------")
+//             document.location.replace('/profile');
+//         } else {
+//             alert(response.statusText);
+//         }
 
-        if (response.ok) {
-            console.log("-------- ACCOUNT CREATED ----------")
-            document.location.replace('/profile');
-        } else {
-            alert(response.statusText);
-        }
+//     }
 
-    }
-
-};
+// };
 
 document.querySelector("#job-button").addEventListener("click", createJobFormHandler);
-document.querySelector("#review-button").addEventListener("click", createReviewFormHandler);
+
